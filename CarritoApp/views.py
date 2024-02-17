3676adcc8cb7d136dcc79fa9f777fbd673484cbb
rauleibanez <83990877+rauleibanez@users.noreply.github.com>
2024-02-16 23:04:43 -0500

    Estructura del Proyecto - Commit Inicial

---
+++ CarritoApp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from CarritoApp.models import Producto
+from CarritoApp.Carrito import Carrito 
+
+# Create your views here.
+def tienda(request):
+	productos = Producto.objects.all()
+	return render(request, "tienda.html", {"productos":productos})
+
+def agregar_producto(request, producto_id):
+	carrito = Carrito(request)
+	producto = Producto.objects.get(id=producto_id)
+	carrito.agregar(producto)
+	return redirect("Tienda")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("Tienda")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("Tienda")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("Tienda")
---
+++ Readme.md
@@ -0,0 +1,27 @@
+## Aplicación de Carrito de Compras 
+
+Carrito de Compras realizado en Django
+
+**Nota:** *Para la totalización se requiere que el usuario este Loggueado*
+
+| BackEnd | FrontEnd | Repositorio de Datos |
+|-|-|
+| Django  | Jinja, Boostrap y Html | SQLite3 |
+
+---
+
+Paqueteria Utilizada en Django
+
+| Paquete | Versión |
+|-|-| 
+| Django | 4.2.6 |
+| Pillow | 10.1.0 |
+
+---
+
+![Alt](imagen1.png)
+
+---
+
+|Raúl Ibáñez M.|
+|-------------------------|   
